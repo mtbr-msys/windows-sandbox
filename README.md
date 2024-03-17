08b2b22fc2ec6ec00e202f72a7a4e6231f1dfcf8
mtbr-msys <m.mt.br.527@gmail.com>
2024-03-18 00:24:32 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Windows SandBox Scripts
+
+[Windows SandBox](https://learn.microsoft.com/ja-jp/windows/security/threat-protection/windows-sandbox/windows-sandbox-overview) で利用するスクリプト類
+
+[WSBファイルについて](https://learn.microsoft.com/ja-jp/windows/security/threat-protection/windows-sandbox/windows-sandbox-configure-using-wsb-file)
---
+++ mount_ro/init.ps1
@@ -0,0 +1,8 @@
+powershell.exe -ExecutionPolicy RemoteSigned  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install sysinternals
+choco install vscode
+choco install chrome-remote-desktop-chrome
+choco install 7zip
+choco install die
+choco install teraterm
+choco install winmerge-jp
