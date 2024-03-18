908acb00a1df680783a6db0f6cb7ddfde9be4479
soralis0912 <33514613+soralis0912@users.noreply.github.com>
2024-03-18 11:50:46 +0900

    fix

--- mount_ro/init.ps1
+++ mount_ro/init.ps1
@@ -1,3 +1,3 @@
-# powershell.exe -ExecutionPolicy RemoteSigned  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+powershell.exe -ExecutionPolicy RemoteSigned  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 start-process -FilePath powershell.exe "-ExecutionPolicy RemoteSigned C:\mount_ro\choco.ps1"
 
