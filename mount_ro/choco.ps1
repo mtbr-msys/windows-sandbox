1a876cda33302c044b0e334bb31ae744e8cee097
soralis0912 <33514613+soralis0912@users.noreply.github.com>
2024-03-18 11:49:57 +0900

    インストールスクリプトを修正

---
+++ mount_ro/choco.ps1
@@ -0,0 +1,2 @@
+C:\ProgramData\chocolatey\bin\choco.exe install C:\mount_ro\choco.config -y
+Pause
--- mount_ro/init.ps1
+++ mount_ro/init.ps1
@@ -1,9 +1,3 @@
-powershell.exe -ExecutionPolicy RemoteSigned  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install sysinternals
-choco install vscode
-choco install chrome-remote-desktop-chrome
-choco install 7zip
-choco install die
-choco install teraterm
-choco install winmerge-jp
-choco install ghidra
+# powershell.exe -ExecutionPolicy RemoteSigned  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+start-process -FilePath powershell.exe "-ExecutionPolicy RemoteSigned C:\mount_ro\choco.ps1"
+Pause
